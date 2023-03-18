2b3bd61f18bf05014bbfe9588888a162dc0e9698
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2023-03-18 16:21:50 +0100

    backend commit

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const connectDb = async () => {
     try {
         const conn = await mongoose.connect(`${process.env.MONGOURL}`)
+        console.log(process.env.MONGOURL , 'process.env.MONGOURLprocess.env.MONGOURL')
         console.log(`Conn  :${conn}`.green.underline.bold )
     } catch (error) {
         console.log(`Error  :${error.message}`.yellow.underline.bold)
